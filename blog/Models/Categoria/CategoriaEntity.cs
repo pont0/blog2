a6225856b660416af4a85407ccf7d046ad27e4e0
pont0 <57673174+pont0@users.noreply.github.com>
2020-02-14 22:28:34 -0300

    commit2

---
+++ blog/Models/Categoria/CategoriaEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blog.Models.Categoria
+{
+    public class CategoriaEntity
+    {
+        public string Nome { get; set; }
+    }
+}
---
+++ blog/Models/Postagens/PostagemEntity.cs
@@ -0,0 +1,17 @@
+﻿using blog.Models.Autor;
+using blog.Models.Categoria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blog.Models.Postagens
+{
+    public class PostagemEntity
+    {
+        public string Titulo { get; set; }
+        public AutorEntity Autor { get; set; }
+
+        public CategoriaEntity Categoria { get; set; }
+    }
+}
